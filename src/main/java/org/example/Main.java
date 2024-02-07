7a716a7f463e1242c88c9ef60b6ded255f223e20
Oualid Ezzoubeidi <siszko37@gmail.com>
2024-02-07 10:10:15 +0100

    setting up the game window size

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
